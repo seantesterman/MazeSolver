4496f47ded0fdfd8346947db9aae1202fa42e8c8
seantesterman <seantesterman@gmail.com>
2024-10-13 22:58:57 -0600

    8.0 Draw Maze

--- graphics.py
+++ graphics.py
@@ -6,10 +6,9 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="#d9d9d9", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -19,13 +18,14 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
-        print("window closed")
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
 
-    def draw_line(self, line, fill_color="black"):
-        line.draw(self.__canvas, fill_color)
 
 class Point:
     def __init__(self, x, y):
@@ -33,15 +33,16 @@ class Point:
         self.y = y
 
 
-
 class Line:
     def __init__(
-            self,
-            p1,
-            p2,            
+        self,
+        p1,
+        p2,
     ):
         self.p1 = p1
         self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill="black", width=2)
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
